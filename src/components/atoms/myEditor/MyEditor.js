1869d26a885ba407d5d084a67e65218f6bc21438
Christian Asivido <christian.asivido@economicmodeling.com>
2020-10-05 21:09:26 -0700

    Update packages

---
+++ src/components/atoms/myEditor/MyEditor.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Editor, EditorState} from 'draft-js';
+
+function MyEditor() {
+  const [editorState, setEditorState] = React.useState(
+    EditorState.createEmpty()
+  );
+
+  const editor = React.useRef(null);
+
+  function focusEditor() {
+    editor.current.focus();
+  }
+
+  React.useEffect(() => {
+    focusEditor()
+  }, []);
+
+  return (
+    <div onClick={focusEditor}>
+      <Editor
+        ref={editor}
+        editorState={editorState}
+        onChange={editorState => setEditorState(editorState)}
+      />
+    </div>
+  );
+}
+
+export default MyEditor;
